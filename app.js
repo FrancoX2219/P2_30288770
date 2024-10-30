7a0151b380c8d84f51efeaf3df3e606885fb5693
FrancoX2219 <francoxjavi19@gmail.com>
2024-10-30 11:44:18 -0400

    p2_30288770
    tarea

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug'); // Asegúrate de usar 'pug'
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.get('/hello', function(req, res) {
+   res.send('Hola Mundo,Nombre:Franco Javier Vasquez Gonzales, Cedula:30.288,770,Seccion:3');
+});
+
+module.exports = app;
