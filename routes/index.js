97db095db08c3257749036e1de291b93ef21d2a1
FrancoX2219 <francoxjavi19@gmail.com>
2024-10-31 19:24:17 -0400

    task1
    tarea nueva

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Escuela de Artes Plásticas' });
 });
+
 
 module.exports = router;
